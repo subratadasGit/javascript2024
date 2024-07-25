909177e5f1ceb19d2b8dd81e173779ff387146ca
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-07-25 17:28:06 +0000

    1.2

---
+++ JavaScript/js_conditional.js
@@ -0,0 +1,34 @@
+// Conditional Operator
+
+let number = -4;
+
+if(number>0){
+    console.log("Number is Positive "+number);
+}
+else if(number<0){
+    console.log("Number is Negative "+number);
+}
+else{
+    console.log("number is zero "+number);
+}
+
+// Ternary Operator
+
+let marks = 40;
+let result = (marks>40) ? "pass" : "Fail";
+console.log(result);
+
+// ternary operator example 2
+
+let result2 = (number>0) ? "Number is Positive "+number : (number<0) ? "Number is Negative "+number : "number is zero "+number ;
+
+console.log(result2);
+
+// Function through ternary operator 
+
+function human(){
+    let age = 60 ;
+    let result3 = (age > 59) ? "Senior Citizen" : "Not a Senior Citizen" ;
+    console.log(result3);
+}
+human();
---
+++ JavaScript/loops.js
@@ -0,0 +1,13 @@
+// Loops ---------->
+   
+// 1 . entry control loop  --> 1.  for loop ,  2. while loop
+// 2 . exit control loop   -->  do-while loop
+
+// entry control loop --> For Loop           i = i+1
+
+let x = 30 ;
+
+for(let i = 1 ; i<=x ; i=i*2 ){
+    console.log(i);
+}
+
