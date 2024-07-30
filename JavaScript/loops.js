4fd743ad340508f7a34eb79e6f6b53618f396be3
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-07-30 16:42:57 +0000

    new

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -5,9 +5,22 @@
 
 // entry control loop --> For Loop           i = i+1
 
-let x = 30 ;
+// let x = 30 ;
 
-for(let i = 1 ; i<=x ; i++){
-    console.log(i);
-}
+// for(let i = 1 ; i<=x ; i++){
+//     console.log(i);
+// }
 
+// let num = 20 ;
+// while(num>0){
+//     console.log(num);
+//     num--;
+
+// }
+
+let num = 20;
+do{
+    console.log(num);
+        num--;
+
+}while(num>0)
