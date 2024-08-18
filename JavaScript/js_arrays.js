a706571c661730a03241382efefbad0772b2147a
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-08-18 04:54:09 +0000

    1

---
+++ JavaScript/js_arrays.js
@@ -0,0 +1,25 @@
+let arr=[12,24,87,6];
+console.log(arr.sort());
+
+let names=["Rajorshi" , "Ayush" , "Pronoy" , "Ananta" , 12 , [12,65]]
+console.log(names.sort());
+
+// let sorted = arr.sort()
+// console.log(sorted);
+
+let a = arr.length;
+let sum = 0;
+for(i=0;i<a;i++){
+    if(a[i]>0 && a[i]<60)
+        {
+       
+}
+
+let avg=(sum/a)
+
+    
+}
+
+
+
+
