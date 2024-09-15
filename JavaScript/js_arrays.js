03a8f9c3300c6021e5536df26a45e0c09652bb9c
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-09-15 04:24:37 +0000

    arrays

--- JavaScript/js_arrays.js
+++ JavaScript/js_arrays.js
@@ -1,25 +1,45 @@
-let arr=[12,24,87,6];
-console.log(arr.sort());
+let names = ["pronoy","ayush","rajorshi"]; // array starts with 0 index 
+console.log(names[2]);
 
-let names=["Rajorshi" , "Ayush" , "Pronoy" , "Ananta" , 12 , [12,65]]
-console.log(names.sort());
+names[0]="akash" ; // assigning the new value
+console.log(names);
 
-// let sorted = arr.sort()
-// console.log(sorted);
+names.push("pronoy") // adding the  new element into the array
+console.log(names);
 
-let a = arr.length;
-let sum = 0;
-for(i=0;i<a;i++){
-    if(a[i]>0 && a[i]<60)
-        {
-       
+names.unshift("ninja hatori") // adding the value (start)
+console.log(names);
+
+console.log(names.length); // total len of the array
+
+// console.log(names.pop()) // delete the last value of the array LIFO(Last In First Out)
+// console.log(names.pop())
+// console.log(names.pop())
+
+names.shift() // delete the zero index value
+console.log(names);
+
+// names.splice(1,3); //(index,del len)
+// console.log(names);
+
+let names2=["shinchan" , "doraemon", "shinchan"]
+ let names3 = names.concat(names2)
+console.log(names3);
+
+let indexofvalue = names3.indexOf("shinchan")
+console.log(indexofvalue);
+
+console.log(names3.findIndex(checkname))
+
+function checkname(_names3){
+    return "shinchan" ;
 }
 
-let avg=(sum/a)
+let num = [22,10,50,30,15,40,45]
 
-    
-}
-
-
-
-
+let value1= num.findIndex(checkname)
+ function checkname(num1) {
+    return num1 > 30  ; // return the first index value if function satisfied by condition
+ }
+ console.log(value1);
+ 
